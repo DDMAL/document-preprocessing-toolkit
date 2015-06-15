cc05433b1e3c9608d6769fd4df43fbfe64cc3f16
Ian Karp <ian.f.karp@gmail.com>
2015-06-15 13:15:21 -0400

    WIP: Store image in stableStaffLineFinder class
    Will keep code organized and hopefully prevent the need to constantly call in the image

---
+++ stable_paths_toolkit/stablePathXCode/unit_test_main.cpp
@@ -0,0 +1,9 @@
+//
+//  unit_test_main.cpp
+//  stablePathXCode
+//
+//  Created by Ian Karp on 2015-06-12.
+//  Copyright (c) 2015 Ian Karp. All rights reserved.
+//
+
+#include "unit_test_main.h"
---
+++ stable_paths_toolkit/stablePathXCode/unit_test_main.h
@@ -0,0 +1,14 @@
+//
+//  unit_test_main.h
+//  stablePathXCode
+//
+//  Created by Ian Karp on 2015-06-12.
+//  Copyright (c) 2015 Ian Karp. All rights reserved.
+//
+
+#ifndef __stablePathXCode__unit_test_main__
+#define __stablePathXCode__unit_test_main__
+
+#include <iostream>
+
+#endif /* defined(__stablePathXCode__unit_test_main__) */
