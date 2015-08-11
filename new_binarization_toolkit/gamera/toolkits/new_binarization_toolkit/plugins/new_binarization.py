46f89161b72765047040625e9e368f8d717b594f
Ian Karp <ian.f.karp@gmail.com>
2015-08-11 15:46:10 -0400

    Added: pythonBinarize
    I will attempt to write the plugin in python before implementing in C++ if necessary

--- new_binarization_toolkit/gamera/toolkits/new_binarization_toolkit/plugins/new_binarization.py
+++ new_binarization_toolkit/gamera/toolkits/new_binarization_toolkit/plugins/new_binarization.py
@@ -1,4 +1,6 @@
 from gamera.plugin import *
+import _arithmetic
+import _convolution
 
 class test(PluginFunction):
     """Fills the entire image with white."""
@@ -11,11 +13,21 @@ class binarize(PluginFunction):
     self_type = ImageType([GREYSCALE])
     return_type = ImageType([GREYSCALE])
 
+class pythonBinarize(PluginFunction):
+    """Attempt to get binarization working using python code"""
+    category = "New Binarization"
+    self_type = ImageType([GREYSCALE])
+    return_type = ImageType([GREYSCALE])
+    pure_python = True
+    def __call__(self, scale = 1.0):
+        return self
+    __call__ = staticmethod(__call__)
+
 class NewBinarization(PluginModule):
     cpp_headers=["new_binarization.hpp"]
     cpp_namespace=["Gamera"]
     category = "New_binarization_toolkit"
-    functions = [test, binarize]
+    functions = [test, binarize, pythonBinarize]
     author = "Your name here"
     url = "Your URL here"
 module = NewBinarization()
--- new_binarization_toolkit/include/plugins/new_binarization.hpp
+++ new_binarization_toolkit/include/plugins/new_binarization.hpp
@@ -29,12 +29,7 @@ template <class T>
 GreyScaleImageView* binarize(T& image)
 {
     GreyScaleImageData *img_data = new GreyScaleImageData(image.size());
-    GreyScaleImageView *img_view = new GreyScaleImageView(*img_data);
-    
-//    BImage src(image.width(), image.height()), edges(image.width(), image.height());
-//    
-//    edges = 0;
-//    cannyEdgeImage(srcImageRange(image), destImage(edges), 0.8, 4.0, 1);
+    GreyScaleImageView *img_view = new GreyScaleImageView(*img_data); 
     
     img_view = canny_edge_image(image, 0.8, 4);
     return img_view;
