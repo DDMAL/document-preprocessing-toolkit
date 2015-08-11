10a15bd003f5bd25c87c76fa2522a9eca0ba3dab
Ian Karp <ian.f.karp@gmail.com>
2015-08-11 16:54:35 -0400

    Added: Laplacian of Gaussian implemented

--- new_binarization_toolkit/gamera/toolkits/new_binarization_toolkit/plugins/new_binarization.py
+++ new_binarization_toolkit/gamera/toolkits/new_binarization_toolkit/plugins/new_binarization.py
@@ -1,6 +1,7 @@
 from gamera.plugin import *
 import _arithmetic
 import _convolution
+import _edgedetect
 
 class test(PluginFunction):
     """Fills the entire image with white."""
@@ -18,9 +19,22 @@ class pythonBinarize(PluginFunction):
     category = "New Binarization"
     self_type = ImageType([GREYSCALE])
     return_type = ImageType([GREYSCALE])
+    args = Args([Real("scale", [0, 1e300], default = 0.8), Real("gradient_threshold", [0, 1e300], default = 4.0)])
     pure_python = True
-    def __call__(self, scale = 1.0):
-        return self
+    def __call__(self, scale, gradient_threshold):
+        canny_image = _edgedetect.canny_edge_image(self, scale, gradient_threshold)
+        """
+            LAPLACIAN OF GAUSSIAN (recreated from convolution.py)
+        """
+        smooth = _convolution.GaussianKernel(scale)
+        deriv = _convolution.GaussianDerivativeKernel(scale, 2)
+        fp = self.to_float()
+        tmp = fp.convolve_x(deriv)
+        tmp_x = tmp.convolve_y(smooth)
+        tmp = fp.convolve_x(smooth)
+        tmp_y = tmp.convolve_y(deriv)
+        result = _arithmetic.add_images(tmp_x, tmp_y, False)
+        return result
     __call__ = staticmethod(__call__)
 
 class NewBinarization(PluginModule):
